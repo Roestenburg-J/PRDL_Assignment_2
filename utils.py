14698cd44712a20648e96b1215b25fab48b03039
Nick Dijkhuizen <nick_dijkhuizen@hotmail.nl>
2024-01-03 17:13:47 +0100

    create utils.py
    some auxiliary functions to process the data for model training

---
+++ utils.py
@@ -0,0 +1,36 @@
+#merge all files into one dataset
+import os
+import h5py
+dir_path = r'../pdata/Intra/train/'
+
+def get_label(filename):
+    if 'rest' in filename:
+        label = 0
+    elif 'math' in filename:
+        label = 1
+    elif 'memory' in filename:
+        label = 2
+    elif 'motor' in filename:
+        label = 3
+    return label
+
+def get_all_matrices(dir_path):
+    dataset = []
+    labels = []
+    for filename in os.listdir(dir_path):
+        if filename.endswith(".h5"):
+            filename_path = dir_path + filename
+            with h5py.File(filename_path, 'r') as f:
+                dataset_name = get_dataset_name(filename_path)
+                label = get_label(filename)
+                matrix = f.get(dataset_name)[()]
+                dataset.append(matrix)
+                labels.append(label)
+              
+
+    return dataset, labels
+
+dataset = get_all_matrices(dir_path)[0]
+# subjects x sensors x time
+dataset_X = np.stack(dataset) 
+labels = get_all_matrices(dir_path)[1]
